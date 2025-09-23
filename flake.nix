5cc31e95b49363b979b8e19a85e4f1f7b41b6238
mochie~! <187453775+mochienya@users.noreply.github.com>
2025-09-24 00:37:17 +0200

    init flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.fenix.overlays.default ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (fenix.latest.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              fenix.rust-analyzer
+            ];
+            RUST_SRC_PATH = pkgs.fenix.stable.rust-src;
+          };
+        };
+    };
+}
