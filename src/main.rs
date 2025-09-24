ce6b460f6cdfb10b2af4d17e7c1d328e5a093cc0
Cassie <37855219+CodeF53@users.noreply.github.com>
2025-09-23 21:17:01 -0600

    image processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod process;
+use process::process;
+
 fn main() {
-    println!("Hello, world!");
+    let in_image = image::open("crafting_table_front.png").expect("you should exist bud");
+    let out_image = process(in_image, true, false);
+
+    out_image.save("scaled.png").unwrap();
 }
