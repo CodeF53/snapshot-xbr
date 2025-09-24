7826039b8aaafe67c543c5ce70c9e2fcfa5c03ed
Cassie <37855219+CodeF53@users.noreply.github.com>
2025-09-24 03:45:13 -0600

    write pseudocode for remaining functionality and yeet old ts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+mod find_pack_format;
 mod mojang_api;
 mod process;
-mod find_pack_format;
 
 #[tokio::main]
 async fn main() {
-    let version_id = "1.21.6";
-    mojang_api::get_client_files(version_id).await.unwrap();
+	let version_id = "1.21.6";
+	mojang_api::get_client_files(version_id).await.unwrap();
 
-    println!("{version_id} has pack format {}", find_pack_format::find_pack_format().unwrap())
+	// unpack zip /assets/minecraft/textures
+	// loop through entries
+	//    if (entry is mcmeta) save_to ./out/{version_id}/entry.path
+	//    img = load_png
+	//    if (/\/(?:block|optifine|painting)\//.exec(path)) wrap = true
+	//    if (/\/(?:model|entity)\//.exec(path)) relayer = true
+	//    out_img = process::process(img, wrap, relayer)
+	//    out_img = oxipng(out_img)
+	//    save_to ./out/{version_id}/entry.path
+
+	// copy static files to ./out/{version_id}
+	// in copied pack.mcmeta change pack_format from -1 to find_pack_format::find_pack_format().unwrap()
+	let pack_format = find_pack_format::find_pack_format().unwrap();
+
+	// zip everything
+
+	// maybe auto upload to modrinth
 }
