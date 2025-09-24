d421d09dc3c5cfca233c682cf542e5606976c761
Cassie <37855219+CodeF53@users.noreply.github.com>
2025-09-24 03:28:56 -0600

    add thing to m,ain

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ mod mojang_api;
 mod process;
 mod find_pack_format;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let version_id = "1.21.6";
+    mojang_api::get_client_files(version_id).await.unwrap();
+
+    println!("{version_id} has pack format {}", find_pack_format::find_pack_format().unwrap())
 }
